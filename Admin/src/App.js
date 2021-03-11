9a3edb54871d0b598f352b3aa958b8ae2d1338d7
KTarun003 <tarunreddykreddy@gmail.com>
2021-03-11 22:00:32 +0530

    Added Pages and Navigation

--- Admin/src/App.js
+++ Admin/src/App.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import Navbar from "./components/navbar";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Ingredients from "./pages/Ingredients";
+import Places from "./pages/Places";
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
-const App = () => {
-	return (
-		<div className='app'>
-			<h1>React Electron Boilerplate</h1>
-			<p>This is a simple boilerplate for using React with Electron</p>
-		</div>
-	)
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div>
+					<Navbar />
+					<Switch>
+						<Route path="/blog">
+							<Blog />
+						</Route>
+						<Route path="/ingredients">
+							<Ingredients />
+						</Route>
+						<Route path="/places">
+							<Places />
+						</Route>
+						<Route path="/">
+							<Home />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
+
+		);
+	}
 }
-
 export default App
---
+++ Admin/src/pages/Ingredients/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Ingredients extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Ingredients Page</h1>
+			</div>
+		);
+	}
+}
+
+export default Ingredients;
